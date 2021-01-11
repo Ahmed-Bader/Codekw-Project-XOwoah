56a57d08cc0ae10c7be26e6dfc860a67eecfb649
Ahmed the Green <67065998+Ahmed-Bader@users.noreply.github.com>
2021-01-11 12:26:44 +0300

    setting up the design for the app

--- XO-Woah/XO-Woah/ContentView.swift
+++ XO-Woah/XO-Woah/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedsize = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Welcome to X-O-Woah!")
+                .font(.largeTitle)
+                .padding()
+            NavigationView{
+                VStack(alignment: .center){
+                    TextField("enter the size of the Board", text: $selectedsize)
+                        .font(.title)
+                        .padding()
+                    
+                    Button(action: {
+                        NavigationLink(
+                            destination: /*@START_MENU_TOKEN@*/Text("Destination")/*@END_MENU_TOKEN@*/,
+                            label: {
+                                /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+                            })
+                    }
+                           ,label: {
+                        Text("Begin!")
+                            .frame(width: 150, height: 50, alignment: .center)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    })
+                }
+            }
+        }
     }
 }
 
@@ -19,3 +45,21 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//        VStack {
+//            Text("Welcome to X-O-Woah!")
+//                .padding()
+//
+//            //button loop for the board of the X-O Game
+//            ForEach (0..<3/*size of game*/){size in // row loop
+//                HStack{
+//                    ForEach(0..<3){size in /*loop for each column in one row (basically the cells*/
+//                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+//                            /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+//                        })
+//
+//                    }
+//                }
+//            }
+//        }
