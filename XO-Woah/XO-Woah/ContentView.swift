75f8245e58a9b1e14d83267ea5564c85c47226d9
Ahmed the Green <67065998+Ahmed-Bader@users.noreply.github.com>
2021-01-11 11:19:30 +0300

    innit commit

---
+++ XO-Woah/XO-Woah/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XO-Woah
+//
+//  Created by Eng.Ahmed Bader on 11/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XO-Woah/XO-Woah/XO_WoahApp.swift
@@ -0,0 +1,17 @@
+//
+//  XO_WoahApp.swift
+//  XO-Woah
+//
+//  Created by Eng.Ahmed Bader on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct XO_WoahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
